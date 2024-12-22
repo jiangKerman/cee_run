d6054b5b00acdff95e7feb098f15207b59c13fd4
jiangKerman <jiangkerman@outlook.com>
2024-12-22 11:13:52 +0800

    Initial commit

---
+++ src/scenes/Game.js
@@ -0,0 +1,28 @@
+import { Scene } from 'phaser';
+
+export class Game extends Scene
+{
+    constructor ()
+    {
+        super('Game');
+    }
+
+    create ()
+    {
+        this.cameras.main.setBackgroundColor(0x00ff00);
+
+        this.add.image(512, 384, 'background').setAlpha(0.5);
+
+        this.add.text(512, 384, '这是一个跑酷游戏，主角是一只平菇，\n 但是游戏还没做完', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('GameOver');
+
+        });
+    }
+}
---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,30 @@
+import { Scene } from 'phaser';
+
+export class MainMenu extends Scene
+{
+    constructor ()
+    {
+        super('MainMenu');
+    }
+
+    create ()
+    {
+        this.add.image(512, 384, 'background');
+
+        this.add.image(512, 300, 'logo',);
+        // this.add.image(512, 300, '平菇');
+
+
+        this.add.text(512, 460, '开始跑！', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('Game');
+
+        });
+    }
+}
